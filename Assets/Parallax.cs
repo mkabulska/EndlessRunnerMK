53c32d1b642211d17c073956ab3e130eca3c997c
mkabulska <mkabulska2023@elam.co.uk>
2023-10-11 11:49:27 +0100

    finished bg
    parallaxing bg

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject player;
+    Renderer rend;
+
+    float playerStartPos;
+    public float speed = 0.5f;
+
     void Start()
     {
-        
+
+        player = GameObject.Find("Player");
+        rend = GetComponent<Renderer>();
+        playerStartPos = player.transform.position.x;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float offset = (player.transform.position.x - playerStartPos) * speed;
+
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0f));
     }
 }
