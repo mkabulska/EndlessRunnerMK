0c3f67e2792ea6ece0be56ef97d8713dcd45f7f3
mkabulska <mkabulska2023@elam.co.uk>
2023-10-13 16:14:43 +0100

    finalish
    finalish version of game for submmition

---
+++ Assets/Script/music.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music : MonoBehaviour
+{
+    public AudioClip backgroundMusic;
+    public AudioSource musicMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
